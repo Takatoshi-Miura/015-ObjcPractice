e45d60728a32f5757805d6bb7841b8d6424160ae
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-21 08:19:12 +0900

    [Add] カスタムクラスHelloを追加。

---
+++ 015-ObjcPractice/Hello.h
@@ -0,0 +1,12 @@
+//
+//  Hello.h
+//  015-ObjcPractice
+//
+//  Created by Takatoshi Miura on 2020/10/21.
+//
+
+#ifndef Hello_h
+#define Hello_h
+
+
+#endif /* Hello_h */
---
+++ 015-ObjcPractice/Hello.m
@@ -0,0 +1,8 @@
+//
+//  Hello.m
+//  015-ObjcPractice
+//
+//  Created by Takatoshi Miura on 2020/10/21.
+//
+
+#import <Foundation/Foundation.h>
