ac1d623a91352739063b9aa5b1cdb0e5ee5b7cd7
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-22 08:22:46 +0900

    [Add] TomboViewControllerを追加。(タッチイベント勉強用)

---
+++ 015-ObjcPractice/TomboViewController.h
@@ -0,0 +1,13 @@
+//
+//  TomboViewController.h
+//  015-ObjcPractice
+//
+//  Created by Takatoshi Miura on 2020/10/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TomboViewController : UIViewController
+
+
+@end
---
+++ 015-ObjcPractice/TomboViewController.m
@@ -0,0 +1,19 @@
+//
+//  TomboViewController.m
+//  015-ObjcPractice
+//
+//  Created by Takatoshi Miura on 2020/10/22.
+//
+
+#import "TomboViewController.h"
+
+@interface TomboViewController()
+
+
+@end
+
+
+@implementation TomboViewController
+
+
+@end
