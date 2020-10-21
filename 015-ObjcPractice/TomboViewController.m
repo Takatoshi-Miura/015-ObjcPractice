8309956590f349e6ac710673b04d98f7546ef61d
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-22 08:41:31 +0900

    [Update] TomboViewControllerにて、タップするとトンボがランダムな位置に移動する処理を実装。

--- 015-ObjcPractice/TomboViewController.m
+++ 015-ObjcPractice/TomboViewController.m
@@ -19,8 +19,20 @@
 
 @implementation TomboViewController
 
-// メソッドの宣言
+// トンボをタップした時の処理
 - (IBAction)tapTombo:(UITapGestureRecognizer *)sender {
+    // サイズの取得
+    CGFloat tomboWidth  = _tomboImage.bounds.size.width;
+    CGFloat tomboHeight = _tomboImage.bounds.size.height;
+    
+    // 移動可能な領域のサイズ
+    NSInteger width  = floor(self.view.bounds.size.width - tomboWidth);
+    NSInteger height = floor(self.view.bounds.size.height - tomboHeight);
+    
+    // ランダムな位置にトンボを移動
+    CGFloat x = (arc4random() % width) + tomboWidth/2;
+    CGFloat y = (arc4random() % height) + tomboHeight/2;
+    _tomboImage.center = CGPointMake(x, y);
 }
 
 @end
